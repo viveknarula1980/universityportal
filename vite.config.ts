123aaac674995dfcf44da351d7c489760581a73b
Anupam <yourgithubemail@gmail.com>
2026-01-07 12:16:07 +0530

    saa

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,8 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
