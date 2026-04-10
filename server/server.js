89023e354499db4e2e1bf9a15fe21666fde780ef
anupamig <hrishihunde@example.com>
2026-04-10 15:48:27 +0530

    cors issue

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ app.use(cors({
     callback(null, true);
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-university-slug']
 }));
 app.use(express.json({ limit: '10mb' }));
